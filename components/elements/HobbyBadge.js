ec7a50ab84ee3977d7cf076551b45c834c9c8c19
Lucas Su <lijie.su93@gmail.com>
2024-01-16 00:23:16 +0800

    brand name font change, badge font to be not bold.

--- components/elements/HobbyBadge.js
+++ components/elements/HobbyBadge.js
@@ -4,7 +4,7 @@ export default function HobbyBadge({ name, icon }) {
     return (
         <div className="col">
             <div class="d-flex justify-content-center" style = {{height:"25px"}}>
-                <div class="d-flex align-items-center justify-content-center bg-dark bg-opacity-50 rounded-start-3 px-2 text-nowrap fw-bold shadow-sm h-100 justify-content-center">{name}</div>
+                <div class="d-flex align-items-center justify-content-center bg-dark bg-opacity-50 rounded-start-3 px-2 text-nowrap shadow-sm h-100 justify-content-center">{name}</div>
                 <div class="d-flex bg-light rounded-end-3 shadow-sm h-100">
                     <i class={`fa-solid text-gray-light fs-7 ${icon} px-2 align-self-center`} ></i>
                 </div>
