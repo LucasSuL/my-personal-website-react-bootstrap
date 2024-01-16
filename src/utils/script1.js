e3413d9e8b5af86c5a704709ee4c9e949b50e84c
Lucas Su <lijie.su93@gmail.com>
2024-01-17 00:47:09 +0800

    improve code for transparent navbar

--- src/utils/script1.js
+++ src/utils/script1.js
@@ -5,33 +5,23 @@ document.addEventListener('DOMContentLoaded', function () {
     // set anchor offset
     const navHeight = navElement.offsetHeight;
     document.documentElement.style.setProperty('--scroll-padding', navHeight - 1 + "px");
+    let isNavbarExpanded = false;
 
     // set transparent navbar if window is on the top
     window.addEventListener('scroll', () => {
-        const isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
-        var scrollPosition = window.scrollY;
+        isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
 
-        if (!isNavbarExpanded && scrollPosition === 0) { // make sure navbar is not expanded
-            navElement.style.setProperty('background-color', 'transparent');
-        } else {
-            navElement.style.backgroundColor = 'rgba(33, 37, 41)';
+        if (!isNavbarExpanded) {
+            const scrollPosition = window.scrollY;
+            navElement.style.setProperty('background-color', scrollPosition === 0 ? 'transparent' : '#212529');
         }
         navElement.style.setProperty('transition', '0.5s');
     });
 
     // handle navbar toggle button click
     navbarToggle.addEventListener('click', () => {
-        // Check if the navbar is expanded or collapsed
-        const isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
-
-        // Set the background color based on the navbar state
-        if (isNavbarExpanded) {
-            navElement.style.backgroundColor = 'rgba(33, 37, 41, 0.95)'; // Change to desired color
-        } else {
-            // Set to transparent or any other color when the navbar is collapsed
-            // const scrollPosition = window.scrollY;
-            // navElement.style.setProperty('background-color', scrollPosition === 0 ? 'transparent' : '#212529');
-        }
+        isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
+        navElement.style.setProperty('background-color', isNavbarExpanded ?  '#212529' : 'transparent');
     });
 
     // Listen for mouseleave event on the navbar
@@ -53,6 +43,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 
+
+    /*
+    nav-link indicator
+    */
     var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
     var sections = document.querySelectorAll('section');
     var isScrollingDisabled = false;
@@ -72,24 +66,21 @@ document.addEventListener('DOMContentLoaded', function () {
             // scroll to the corresponding part
             sections[sectionIndex].scrollIntoView({ behavior: 'smooth' });
 
-            // 启用滚动事件监听器，延迟 500 毫秒以防止滚动事件立即触发
             setTimeout(function () {
                 isScrollingDisabled = false;
             }, 800);
         });
     });
 
-    // 添加滚动事件监听器
     window.addEventListener('scroll', function () {
         if (!isScrollingDisabled) {
             var currentScroll = window.scrollY;
 
             sections.forEach(function (section, index) {
-                var sectionTop = section.offsetTop - 300; // 考虑到导航栏的高度
+                var sectionTop = section.offsetTop - 300;
                 var sectionBottom = sectionTop + section.offsetHeight;
 
                 if (currentScroll >= sectionTop && currentScroll < sectionBottom) {
-                    // 设置当前可见的部分对应的导航链接为 'active'
                     navLinks.forEach(function (innerLink) {
                         innerLink.classList.remove('active');
                     });
