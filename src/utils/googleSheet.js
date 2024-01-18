bd36fb0dde942658c48f172728d84d3e99edee49
Lucas Su <lijie.su93@gmail.com>
2024-01-19 01:50:54 +0800

    handled google sheet submit

--- src/utils/googleSheet.js
+++ src/utils/googleSheet.js
@@ -2,7 +2,7 @@ export function submitToGoogleSheet() {
 
     const scriptURL = 'https://script.google.com/macros/s/AKfycbwHEK6y9AmSg7149c4LnqNfQxHGq-HVLiY88YrkKPsHY-UJ5RIB3cWkrRVej0IV6bE/exec'
     const form = document.forms['submit-to-google-sheet']
-console.log(form)
+    
     form.addEventListener('submit', e => {
         e.preventDefault()
         fetch(scriptURL, { method: 'POST', body: new FormData(form) })
