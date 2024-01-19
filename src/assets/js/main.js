04f85bcd1b6f74f82d8772d4648915dbb1fa9ddb
Lucas Su <lijie.su93@gmail.com>
2024-01-19 12:58:12 +0800

    add modal

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,5 @@
 import "./modules/nav"
 import submitGoogle from "./modules/submitGoogleSheet"
-// import toast from "./modules/toasts"
 
 document.addEventListener('DOMContentLoaded', () => {
     submitGoogle();
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -20,7 +20,7 @@ export default function Footer() {
                             <li class="mb-2"><a href="#articles" class="p-0 text-secondary text-decoration-none">Articles</a></li>
                         </ul>
                     </div>
-                    
+
 
                     <div class="col-sm-4 col-md-2 mb-3">
                         <h5>About</h5>
@@ -54,19 +54,23 @@ export default function Footer() {
                                     <label for="exampleFormControlTextarea1" class="form-label visually-hidden">Example textarea</label>
                                     <textarea class="form-control" name="Message" id="exampleFormControlTextarea1" rows="3"></textarea>
                                 </div>
-                                <button class="btn btn-primary" id="liveToastBtn" type="submit">Submit</button>
+                                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" type="submit">Submit</button>
                             </div>
                         </form>
-                        <div class="toast-container position-fixed bottom-0 end-0 p-3">
-                            <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                                <div class="toast-header">
-                                    <img src="..." class="rounded me-2" alt="..."/>
-                                        <strong class="me-auto">Bootstrap</strong>
-                                        <small>11 mins ago</small>
-                                        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                                </div>
-                                <div class="toast-body">
-                                    Hello, world! This is a toast message.
+
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        ...
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
